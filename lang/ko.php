d70cd5c5f3d63f28f47a0bfa4fb96505b2bd5e31
kkigomi <kkigomi@duck.com>
2024-01-02 23:05:01 +0900

    작은 코드들 정리

--- lang/ko.php
+++ lang/ko.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * 모듈의 한국어 언어팩
+ *
+ * `$lang` 변수에 모든 모듈, 위젯 등이 공유하므로 중복되지않는 접두어를 사용해 지정하는 것이 좋습니다.
+ */
+
 /** @var object $lang */
 $lang->example1_module = '예제1 모듈';
 $lang->example1_enable_module = '모듈 활성화';
